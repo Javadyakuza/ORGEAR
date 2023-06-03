af14fdaa78bda2db938676cd05ee388925ee1675
yakuza <javadyakuzaa@gmail.com>
2023-06-03 19:28:52 +0330

    PROPOSAL_TEST_ADDED

--- test/DeployProposal.test.ts
+++ test/DeployProposal.test.ts
@@ -16,7 +16,8 @@ let ActionTestPersonalDataAddr: Address;
 let ProposalAddr: Address;
 let WalletV3: CreateAccountOutput;
 let signer: Signer;
-
+let WalletV3_2: CreateAccountOutput;
+let signer2: Signer;
 describe("shuold deploy Dao Branch ", async function () {
   before(async () => {
     // checking the codes of contracts are availbale or no
@@ -63,6 +64,18 @@ describe("shuold deploy Dao Branch ", async function () {
     });
     WalletV3.account.prepareMessage;
     console.log("walletv3 : ", WalletV3.account.address.toString());
+    signer2 = (await locklift.keystore.getSigner("1"))!;
+    // making the wallet v3 and the token root  and wallet \
+    // deploying it
+    WalletV3_2 = await locklift.factory.accounts.addNewAccount({
+      type: WalletTypes.WalletV3, // or WalletTypes.HighLoadWallet or WalletTypes.WalletV3,
+      //Value which will send to the new account from a giver
+      value: locklift.utils.toNano(100),
+      //owner publicKey
+      publicKey: signer.publicKey,
+    });
+    WalletV3.account.prepareMessage;
+    console.log("walletv3_2 : ", WalletV3.account.address.toString());
     expect(WalletV3.account.address).to.not.eq(zeroAddress);
     // in this operation we will send the intial supplu to the owner of the root and this will deploya wallet for us and reduces the work that we need to do
     // getting the wallet code
@@ -81,7 +94,7 @@ describe("shuold deploy Dao Branch ", async function () {
         deployer_: zeroAddress,
       },
       constructorParams: {
-        initialSupplyTo: WalletV3.account.address,
+        initialSupplyTo: WalletV3_2.account.address,
         initialSupply: 100,
         deployWalletValue: locklift.utils.toNano(0.1),
         mintDisabled: false,
@@ -94,7 +107,7 @@ describe("shuold deploy Dao Branch ", async function () {
     Tip3voteRootAddr = Tip3voteRoot.address;
     // getting the wallet address
     Tip3voteWalletAddr = (
-      await Tip3voteRoot.methods.walletOf({ answerId: 0, walletOwner: WalletV3.account.address }).call({})
+      await Tip3voteRoot.methods.walletOf({ answerId: 0, walletOwner: WalletV3_2.account.address }).call({})
     ).value0;
     console.log(`VoteRoot : ${Tip3voteRootAddr} \n VoteWallet : ${Tip3voteWalletAddr}`);
     // testing root deployment
@@ -102,7 +115,7 @@ describe("shuold deploy Dao Branch ", async function () {
     const Tip3voteWallet = locklift.factory.getDeployedContract("VoteTokenWallet", Tip3voteWalletAddr);
     // testing the wallet owneer to be the wallet v3
     expect((await Tip3voteWallet.methods.owner({ answerId: 0 }).call({})).value0.toString()).to.eq(
-      WalletV3.account.address.toString(),
+      WalletV3_2.account.address.toString(),
     );
     // testing wallet
     expect((await Tip3voteWallet.methods.balance({ answerId: 0 }).call({})).value0.toString()).to.eq("100");
@@ -188,7 +201,7 @@ describe("shuold deploy Dao Branch ", async function () {
     // testing the date
     expect((await ActionTestPersonalData.methods.age({}).call({})).age.toString()).to.eq("43");
   });
-  it("shuold deploy a proposal from the dao branch ", async function () {
+  it("shuold deploy a proposal by admin from daoBranch ", async function () {
     // fetching tha DaoBranch contract and the action tester
     const DaoBranch = await locklift.factory.getDeployedContract("DAOBranch", DAOBranchAddr);
     const ActionTestPersonalData = await locklift.factory.getDeployedContract(
@@ -218,8 +231,54 @@ describe("shuold deploy Dao Branch ", async function () {
       contract: DAOBranchCon,
       name: "ProposalDeployed" as const, // 'as const' is important thing for type saving
     });
-    console.log("this is the event : \n", ProposalEvents);
+    console.log("this is the proposal id : ", ProposalEvents![0].proposalId);
     // fetching the deployed proposal
-    //   await Proposal = locklift.factory.getDeployedContract("Proposal", ((await DaoBranch.methods.expectedProposalAddress({_proposalId}).call({})).expectedProposalAddress_))
+    const Proposal = await locklift.factory.getDeployedContract(
+      "Proposal",
+      (
+        await DaoBranch.methods.expectedProposalAddress({ _proposalId: ProposalEvents![0].proposalId }).call({})
+      ).expectedProposalAddress_,
+    );
+    expect((await Proposal.methods.PROPOSAL_ID({}).call({})).PROPOSAL_ID).to.eq(ProposalEvents![0].proposalId);
+  });
+  it("shuold deploy a proposal by DAOTokenHolder from daoBranch ", async function () {
+    // fetching tha DaoBranch contract and the action tester
+    const DaoBranch = await locklift.factory.getDeployedContract("DAOBranch", DAOBranchAddr);
+    const ActionTestPersonalData = await locklift.factory.getDeployedContract(
+      "ActionTestPersonalData",
+      ActionTestPersonalDataAddr,
+    );
+    ProposalAction[0].target = ActionTestPersonalDataAddr;
+    ProposalAction[1].target = ActionTestPersonalDataAddr;
+    ProposalAction[0].payload = await ActionTestPersonalData.methods.setAge({ _age: 9 }).encodeInternal();
+    ProposalAction[1].payload = await ActionTestPersonalData.methods.setName({ _name: "hamed" }).encodeInternal();
+    // changing the actions
+    // changing the poroposal configuration tip3 vote address
+    ProposalConfigurationStructure.TIP3_VOTE_ROOT_ADDRESS = Tip3voteRootAddr;
+    const { traceTree: data } = await locklift.tracing.trace(
+      DaoBranch.methods
+        .propose({
+          _ProposalInitConfiguration: ProposalConfigurationStructure,
+          _venomActions: ProposalAction,
+        })
+        .send({
+          from: WalletV3_2.account.address,
+          amount: locklift.utils.toNano(20),
+        }),
+    );
+    // fetching the emmited event reffering to ther propoal deploying
+    const ProposalEvents = data?.findEventsForContract({
+      contract: DAOBranchCon,
+      name: "ProposalDeployed" as const, // 'as const' is important thing for type saving
+    });
+    console.log("this is the proposal id : ", ProposalEvents![0].proposalId);
+    // fetching the deployed proposal
+    const Proposal = await locklift.factory.getDeployedContract(
+      "Proposal",
+      (
+        await DaoBranch.methods.expectedProposalAddress({ _proposalId: ProposalEvents![0].proposalId }).call({})
+      ).expectedProposalAddress_,
+    );
+    expect((await Proposal.methods.PROPOSAL_ID({}).call({})).PROPOSAL_ID).to.eq(ProposalEvents![0].proposalId);
   });
 });
